676718cd4b85d3aaa6a771bb9afbba6c47066349
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-15 17:17:07 -0300

    removing var and echoing directly

--- .github/workflows/scripts/news/mapper/issue-body-mapper.sh
+++ .github/workflows/scripts/news/mapper/issue-body-mapper.sh
@@ -1,5 +1,4 @@
 BODY="$1"
 FIELD="$2"
 echo "Mapping $BODY for $FIELD"
-VALUE=$(echo "$BODY" | grep "$FIELD" | cut -d ':' -f2 | xargs)
-echo $VALUE
+echo "$BODY" | grep "$FIELD" | cut -d ':' -f2 | xargs
