356d97013c75df137b05fd85b34a316b74d5fa84
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-15 17:15:29 -0300

    Fix body mapper syntax

--- .github/workflows/scripts/news/mapper/issue-body-mapper.sh
+++ .github/workflows/scripts/news/mapper/issue-body-mapper.sh
@@ -1,4 +1,5 @@
-$BODY=$1
-$FIELD=$2
+BODY="$1"
+FIELD="$2"
 echo "Mapping $BODY for $FIELD"
-$VALUE=$(echo "$BODY" | grep "$FIELD" | cut -d ':' -f2 | xargs)
+VALUE=$(echo "$BODY" | grep "$FIELD" | cut -d ':' -f2 | xargs)
+echo $VALUE
