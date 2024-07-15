6d36c77624b4c0af3d5f764da6422acbc19f5566
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-15 16:04:08 -0300

    allowing scripts before run job

--- .github/workflows/scripts/news/script-enabler.sh
+++ .github/workflows/scripts/news/script-enabler.sh
@@ -1,16 +1,16 @@
 mappers_dir="./.github/workflows/scripts/news/mapper"
 
 required_scripts=(
-    "get-news-reference.sh"
-    "get-news-pages.sh"
-    "issue-body-mapper.sh",
+    "get-news-reference"
+    "get-news-pages"
+    "issue-body-mapper",
     format-page.sh
 )
 
 for script in "${required_scripts[@]}"; do
     echo "enabling script $script"
-    chmod u+r+x "$mappers_dir/$script"
-    git update-index --chmod=+x "$mappers_dir/$script"
+    chmod u+r+x "$mappers_dir/$script.sh"
+    git update-index --chmod=+x "$mappers_dir/$script.sh"
 done
 echo "News Scripts allowed"
 ls -l
