764e4628339f37645ed17f1d61d87d8ef0571e68
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-15 17:21:24 -0300

    fixed syntax

--- .github/workflows/scripts/news/mapper/format-page.sh
+++ .github/workflows/scripts/news/mapper/format-page.sh
@@ -1,9 +1,9 @@
-$BODY=$1
-$FIELD=$2
+BODY=$1
+FIELD=$2
 echo "Mapping Body => $BODY"
 mappers_dir="./.github/workflows/scripts/news/mapper"
-$MAPPER_SCRIPT = "$mappers_dir/issue-body-mapper.sh"
-$DESCRIPTION= $($MAPPER_SCRIPT "$BODY" "$FIELD")
+MAPPER_SCRIPT = "$mappers_dir/issue-body-mapper.sh"
+DESCRIPTION= $($MAPPER_SCRIPT "$BODY" "$FIELD")
 NEWS_PAGE=$(
     jq -n \
     --arg title "$DESCRIPTION" \
