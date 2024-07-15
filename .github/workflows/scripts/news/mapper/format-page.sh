ba0507ff29c9fb01a14de7ba84cfdea3eff3d4a3
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-15 17:35:24 -0300

    fix = sign

--- .github/workflows/scripts/news/mapper/format-page.sh
+++ .github/workflows/scripts/news/mapper/format-page.sh
@@ -1,8 +1,8 @@
 BODY="$1"
 FIELD="$2"
 echo "Mapping Body => $BODY"
-mappers_dir="./.github/workflows/scripts/news/mapper"
-mapper_script = "$mappers_dir/issue-body-mapper.sh"
+mappers_dir= "./.github/workflows/scripts/news/mapper"
+mapper_script= "$mappers_dir/issue-body-mapper.sh"
 DESCRIPTION= $($mapper_script "$BODY" "$FIELD")
 NEWS_PAGE=$(
     jq -n \
