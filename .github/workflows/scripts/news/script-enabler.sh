330a8b1a61281a3886799936aded3a7e96bb590d
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-15 16:06:08 -0300

    remove commas

--- .github/workflows/scripts/news/script-enabler.sh
+++ .github/workflows/scripts/news/script-enabler.sh
@@ -3,13 +3,13 @@ mappers_dir="./.github/workflows/scripts/news/mapper"
 required_scripts=(
     "get-news-reference"
     "get-news-pages"
-    "issue-body-mapper",
-    format-page.sh
+    "issue-body-mapper"
+    "format-page.sh"
 )
 
 for script in "${required_scripts[@]}"; do
     echo "enabling script $script"
-    chmod u+r+x "$mappers_dir/$script.sh"
+    chmod u+r+x "$mappers_dir/$script"
     git update-index --chmod=+x "$mappers_dir/$script.sh"
 done
 echo "News Scripts allowed"
