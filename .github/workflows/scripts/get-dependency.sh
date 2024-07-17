c98fcf1fa4a9f40fa393842c5ba13bb297c9a489
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-17 16:31:58 -0300

    removing echos

--- .github/workflows/scripts/get-dependency.sh
+++ .github/workflows/scripts/get-dependency.sh
@@ -5,19 +5,12 @@ tempFolder="./.github/workflows/.temp"
 dependencyFile="$DEPENDENCY-$VERSION.jar"
 
 # Find the file and copy it to the current directory if it exists
-echo "Searching for $dependencyFile in $tempFolder"
-echo -e "\n"
-echo "Listing all files in $tempFolder:"
 ls -l $tempFolder
-echo -e "\n"
+
 foundFiles=$(find $tempFolder -type f -name "$dependencyFile")
 
 if [ -n "$foundFiles" ]; then
-    echo "Found file: $foundFiles"
-    echo -e "\n"
     echo $foundFiles
 else
-    echo "File not found."
-    echo -e "\n"
     exit 1
 fi
