47ee6e6d9121c12d2b9ab34715a2e146598d2cba
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-19 16:17:08 -0300

    Remove commit from temp files

--- .github/workflows/scripts/dependency-resolver.sh
+++ .github/workflows/scripts/dependency-resolver.sh
@@ -25,8 +25,6 @@ curl -L -o $tempFolder/$DEPENDENCY-$VERSION.jar $dependencyURL
 tempFile="$tempFolder/$DEPENDENCY-$VERSION.jar"
 if [ -f $tempfile ]; then
     echo "Downloaded $DEPENDENCY-$VERSION.jar successfully."
-    git add "$tempFile"
-    git commit -m "Add $DEPENDENCY-$VERSION.jar depedency"
 else
     echo "Failed to download $DEPENDENCY-$VERSION.jar."
     exit 1
