90f778ffd2d6ac6abc4078c1e0cbd98b4870af29
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-20 12:17:04 -0300

    Fix description script

--- .github/workflows/actions/edit-news-pr-action/write-description-text-from-json.sh
+++ .github/workflows/actions/edit-news-pr-action/write-description-text-from-json.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-resourcePath="resources/news.json"
-lastElementTitle=$(jq '.news[-1].pages[0].title' "$jsonFilePath")
-lastElementThumbnail=$(jq '.news[-1].pages[0].thumbnail' "$jsonFilePath")
-lastElementAuthor=$(jq '.news[-1].pages[0].reference.author' "$jsonFilePath")
-lastElementLink=$(jq '.news[-1].pages[0].reference.link' "$jsonFilePath")
-descriptions=$(jq -r '.news[-1].pages | map(.description) | join(" ")' "$resourcePath")
+cd ./resources
+ls -a
+jsonFilePath="./news.json"
 
-cat << 'EOF'
+jsonContent=$(cat "$jsonFilePath")
+
+echo "news file content: $jsonContent"
+
+lastElementTitle=$(echo "$jsonContent" | jq '.news[-1].pages[0].title')
+lastElementThumbnail=$(echo "$jsonContent" | jq '.news[-1].pages[0].thumbnail')
+lastElementAuthor=$(echo "$jsonContent" | jq '.news[-1].reference.author')
+lastElementLink=$(echo "$jsonContent" | jq '.news[-1].reference.link')
+descriptions=$(echo "$jsonContent" | jq -r '.news[-1].pages | map(.description) | join("\n\n")')
+
+cat << EOF
 ![${lastElementTitle}](${lastElementThumbnail})
 # ${lastElementTitle}
   
