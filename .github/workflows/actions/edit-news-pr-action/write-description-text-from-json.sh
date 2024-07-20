e25eda16bab07ac2ae46351d8522549c060feb77
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-20 18:07:27 -0300

    fix field

--- .github/workflows/actions/edit-news-pr-action/write-description-text-from-json.sh
+++ .github/workflows/actions/edit-news-pr-action/write-description-text-from-json.sh
@@ -11,7 +11,7 @@ echo "news file content: $jsonContent"
 lastElementTitle=$(echo "$jsonContent" | jq '.news[-1].pages[0].title')
 lastElementThumbnail=$(echo "$jsonContent" | jq '.news[-1].pages[0].thumbnail')
 lastElementAuthor=$(echo "$jsonContent" | jq '.news[-1].reference.author')
-lastElementLink=$(echo "$jsonContent" | jq '.news[-1].reference.link')
+lastElementLink=$(echo "$jsonContent" | jq '.news[-1].reference.reference')
 descriptions=$(echo "$jsonContent" | jq -r '.news[-1].pages | map(.description) | join("\n\n")')
 
 cat << EOF
