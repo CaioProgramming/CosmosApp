cac4a4966d8a9941c4d709103972d964be68dfea
Caio Alves Ferreira <caioalvesferreiracontato@gmail.com>
2024-07-20 21:53:19 -0300

    Outputing description

--- .github/workflows/actions/edit-news-pr-action/write-description-text-from-json.sh
+++ .github/workflows/actions/edit-news-pr-action/write-description-text-from-json.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
 cd ./resources
 ls -a
 jsonFilePath="./news.json"
 
 jsonContent=$(cat "$jsonFilePath")
 
-echo "news file content: $jsonContent"
-
 lastElementTitle=$(echo "$jsonContent" | jq '.news[-1].pages[0].title')
-lastElementThumbnail=$(echo "$jsonContent" | jq '.news[-1].pages[0].thumbnail')
+lastElementThumbnail=$(echo "$jsonContent" | jq '.news[-1].pages[0].thumbnailURL')
 lastElementAuthor=$(echo "$jsonContent" | jq '.news[-1].reference.author')
 lastElementLink=$(echo "$jsonContent" | jq '.news[-1].reference.reference')
 descriptions=$(echo "$jsonContent" | jq -r '.news[-1].pages | map(.description) | join("\n\n")')
